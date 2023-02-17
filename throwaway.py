4aa35e9688ccbb64ea53ccac985a5bc7ea19d4a9
Eric Simpson <52686452+ericssimpson@users.noreply.github.com>
2023-02-17 15:42:27 -0800

    asdasd

--- throwaway.py
+++ throwaway.py
@@ -1,5 +1,11 @@
+from owid import catalog
+
+catalog.find('covid')
+
+'''
 sdg_api = requests.get('https://dashboards.sdgindex.org/static/downloads/SDR2022.json', auth=('sdgindex-guest', 'W4zaVqsGPUrV6KaVDFfY'))
 print(sdg_api.status_code)
 
 sdg_data = pd.read_json(json.dumps(sdg_api.json()), dtype=object)
 sdg_data.to_csv('out.csv')
+'''
